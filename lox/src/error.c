7f474d0f7280c7fc5b83ca949dcf3b88c57862aa
Brymer <meneses.brymer@gmail.com>
2022-06-30 22:57:13 +0800

    refactor error reporting

--- lox/src/error.c
+++ lox/src/error.c
@@ -8,43 +8,46 @@
 #include "lox/scanner.h"
 #include "lox/parser.h"
 
-#define COLOR(COLOR, STRING) COLOR STRING ANSI_COLOR_RESET
-#define SET_COLOR(COLOR) printf(COLOR)
+#include "termcolor.h"
 
-#define ANSI_COLOR_RED     "\x1b[31m"
-#define ANSI_COLOR_GREEN   "\x1b[32m"
-#define ANSI_COLOR_YELLOW  "\x1b[33m"
-#define ANSI_COLOR_BLUE    "\x1b[34m"
-#define ANSI_COLOR_MAGENTA "\x1b[35m"
-#define ANSI_COLOR_CYAN    "\x1b[36m"
-#define ANSI_COLOR_RESET   "\x1b[0m"
+#define COLOR(COLOR, STRING) COLOR STRING ANSI_CODE_RESET
+
+static char* init_empty_string(const int size) {
+  char* str = malloc(size * sizeof(char));
+
+  for (int i=0; i<size; i++) {
+    str[i] = ' ';
+  }
+  str[size] = '\0';
+  return str;
+}
 
 
 static void point_error_root(const size_t line_number, const char* source, const size_t begin, const size_t end) {
 
-  char source_context[128];
-  char formatted_source[strlen(source) + strlen(source_context)];
+  char source_context[64];
+  sprintf(source_context, "    %lu| ", line_number);
 
-  sprintf(source_context, "    %lu | ", line_number);
-  sprintf(formatted_source, "%s%s", source_context, source);
+  const unsigned int source_context_size = strlen(source_context);
+  const unsigned int source_size = strlen(source);
 
-  const size_t offsetted_begin = begin + strlen(source_context);
-  const size_t offsetted_end = end + strlen(source_context);
+  char* pointer_str = init_empty_string(source_size);
+  char* pointer_str_offset = init_empty_string(source_context_size);
 
-  char* pointer_str = strdup(formatted_source);
-  for (size_t i=0; i<strlen(pointer_str); i++) {
-
-    if (i>=offsetted_begin && i<= offsetted_end) {
+  for (size_t i=0; i<strlen(source); i++) {
+    if (i>=begin && i<= end) {
       pointer_str[i] = '^';
-    } else {
-      pointer_str[i] = ' ';
-    }
+    } 
   }
 
-  SET_COLOR(ANSI_COLOR_RESET);
-  printf("%s\n", formatted_source);
-  SET_COLOR(ANSI_COLOR_CYAN);
-  printf("%s\n", pointer_str);
+  printf( COLOR(ANSI_CODE_CYAN, "%s"), source_context); 
+  printf("%s\n", source);
+
+  printf("%s", pointer_str_offset);
+  printf( COLOR(ANSI_CODE_CYAN, "%s\n"), pointer_str);
+
+  free(pointer_str);
+  free(pointer_str_offset);
 }
 
 
@@ -53,12 +56,14 @@ void raise_unterminated_string_error(Scanner* s, const char* lexeme) {
 
   point_error_root(s->line, scanner_get_current_line(s), current_pos_rel_line, current_pos_rel_line + strlen(lexeme));
 
-  printf( COLOR(ANSI_COLOR_RED, "  ERROR: ") "Unterminated string, did you forget to place \"?\n");
+  printf( COLOR(ANSI_CODE_RED, "  ERROR: ") "Unterminated string, did you forget to place \"?\n");
+  LOX_HAD_ERROR = true;
 }
 
 void raise_unexpected_character_error(Scanner* s, const char chr) {
   const int current_pos_rel_line = s->current - 1 - s->last_line;
   point_error_root(s->line, scanner_get_current_line(s), current_pos_rel_line, current_pos_rel_line);
 
-  printf( COLOR(ANSI_COLOR_RED, "  ERROR: ") "Unexpected character: %c\n", chr);
+  printf( COLOR(ANSI_CODE_RED, "  ERROR: ") "Unexpected character: %c\n", chr);
+  LOX_HAD_ERROR = true;
 }
