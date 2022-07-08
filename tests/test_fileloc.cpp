1dd3d0b92198de9f56f19c922d586854e3dc3736
Brymer <meneses.brymer@gmail.com>
2022-07-08 18:46:30 +0800

    fix function `fileloc_range`

---
+++ tests/test_fileloc.cpp
@@ -0,0 +1,17 @@
+#include "gtest/gtest.h"
+
+extern "C" {
+ #include "tools/fileloc.h"
+}
+
+TEST(TestUtils, FileLocRange) {
+  
+  FileLoc fl = fileloc_range(2, fileloc_init(5, 3, 5), fileloc_init(10, 25, 30));
+
+  EXPECT_EQ(10, fl.line);
+  EXPECT_EQ(3, fl.start);
+  EXPECT_EQ(30, fl.end);
+
+}
+
+
