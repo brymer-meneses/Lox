7e3ed729e813de5e1f22f2c52343c520c63fa666
Brymer <meneses.brymer@gmail.com>
2022-07-31 17:06:20 +0800

    improve environment test

--- tests/test_environment.rs
+++ tests/test_environment.rs
@@ -15,9 +15,32 @@ fn it_should_define_variables() {
         value: 299792258.0,
     };
 
+
     env.define("PI_VALUE".to_string(), pi_value.clone());
     env.define("SPEED_OF_LIGHT".to_string(), speed_of_light.clone());
 
+    assert_eq!(2, env.length());
     assert_eq!(env.retrieve("PI_VALUE").unwrap(), pi_value);
     assert_eq!(env.retrieve("SPEED_OF_LIGHT").unwrap(), speed_of_light);
 }
+
+#[test]
+fn it_should_reassign_variables() {
+    let mut env = Environment::new(None);
+    let location = SourceLocation::new_single_line(0, 0, 0);
+    let num1 = LoxObject::Nil {location};
+    let num2 = LoxObject::Number {
+        location,
+        value: 2.71828,
+    };
+
+    env.define("number".to_string(), num1.clone());
+
+    assert_eq!(env.retrieve("number").unwrap(), num1);
+
+    env.assign("number".to_string(), num2.clone());
+
+    assert_eq!(env.retrieve("number").unwrap(), num2);
+    assert_eq!(1, env.length());
+}
+
